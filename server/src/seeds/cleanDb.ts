a1c0bc8e916c2f07a55d708904008cb1e1a805f5
Tyler Hayman <tyler.hayman@icloud.com>
2025-05-04 12:16:50 -0600

    changing it back

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,9 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    const model = models[modelName];
-    if (!model || !model.db) {
-      throw new Error(`Model or database connection for ${modelName} is undefined.`);
-    }
-
-    const modelExists = await model.db.db.listCollections({
+    let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
-    }).toArray();
+    }).toArray()
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
